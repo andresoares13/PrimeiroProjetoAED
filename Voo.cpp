b663f8de253931d10774c4e9bc8477292cf3ddce
Andre <andre.soaestc13@gmail.com>
2021-12-14 22:52:47 +0000

    sofia

--- Voo.cpp
+++ Voo.cpp
@@ -17,7 +17,73 @@ int Voo::getDuration() const {
     return duration;
 }
 
+string Voo::getOrigem() const {
+    return origem;
+}
+
+string Voo::getDestino() const {
+    return destino;
+}
 
 int Voo::getCapacity() const {
     return capacity;
+}
+
+int Voo::getProfit() const {
+    int profit;
+    int bilhetesvendidos = capacity - ticketsAvailable();
+
+    profit = bilhetesvendidos * getTicketPrice();
+
+    return profit;
+}
+
+int Voo::getTicketPrice() const {
+    return ticketprice;
+}
+
+int Voo::ticketsAvailable() const {
+    return capacity - passageiros.size();
+}
+
+list<Passageiro> Voo::getPassageiros() const {
+    return list<Passageiro>();
+}
+
+bool Voo::addPassageiro(Passageiro &p) {
+
+    if(ticketsAvailable() == 0)//verificar se está cheio
+    {
+        return false;
+    }
+    else
+    {
+        passageiros.push_back(p);
+        return true;
+    }
+}
+
+bool Voo::removePassageiro(int ID) {
+
+
+    for(int i = 0; i < passageiros.size(); i++)
+    {
+        if(passageiros[i].getID() == ID)
+        {
+            passageiros.erase(passageiros.begin() + i);
+            return true;
+        }
+    }
+
+    return false; //se o passageiro não estiver
+}
+
+Passageiro &Voo::findPassageiro(int ID) {
+    for(int i = 0; i < passageiros.size(); i++)
+    {
+        if(passageiros[i].getID() == ID)
+        {
+            //return ;
+        }
+    }
 }
