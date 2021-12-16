28b32ef881198ca2324ae4bcffe7b0c43e1672e5
Barbara <barbarafilipacarvalho08@gmail.com>
2021-12-16 22:24:20 +0000

    Aeroporto

--- main.cpp
+++ main.cpp
@@ -11,7 +11,21 @@
 using namespace std;
 int main() {
     Aeroporto A;
-    /*
+
+    vector<float> v1={9.10, 9.45, 9.50};
+    vector<float> v2={10.20, 11.40};
+    vector<float> v3={13.45};
+    LocalTransporte l1("metro", 1.2, v1, true);
+    LocalTransporte l2("comboio", 0.8, v2, true);
+    LocalTransporte l3("autocarro", 1.0, v3, false);
+    A.addLocal(l1);
+    A.addLocal(l2);
+    A.addLocal(l3);
+    A.printLocais();
+    vector<LocalTransporte> r1=A.getLocais();
+    vector<LocalTransporte> r2=A.getLocais("metro");
+
+/*
     Voo v(1,"p",2,"o","o",500,20);
     Passageiro p(1,1,true,true);
     Passageiro p1(1,1,true,false);
@@ -29,6 +43,9 @@ int main() {
         car.getBagagem()[i].get();
     }
      */
+
+
+
     return 0;
 
 }
